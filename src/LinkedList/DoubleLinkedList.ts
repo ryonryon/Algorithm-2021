be54c160412af4113f653a3d89ce1da97e9f34cd
Ryo Togashi <ryo.togashi.ca@gmail.com>
2022-06-09 08:20:44 -0700

    Update DoubleLinkedList

--- src/LinkedList/DoubleLinkedList.ts
+++ src/LinkedList/DoubleLinkedList.ts
@@ -1,100 +1,120 @@
-class ListNode {
-  value: number;
-  prev: ListNode | null;
-  next: ListNode | null;
+export default class DoubleLinkedList {
+  root: LinkedListNode | null;
 
-  constructor(value: number, prev?: ListNode | null, next?: ListNode | null) {
-    this.value = value;
-    this.prev = prev ?? null;
-    this.next = next ?? null;
+  constructor() {
+    this.root = null;
+  }
+
+  get(index: number): number {
+    try {
+      const node = this.getNodeAt(index);
+
+      return node.value;
+    } catch (e) {
+      return -1;
+    }
+  }
+
+  addAtHead(val: number): void {
+    if (!this.root) {
+      this.root = new LinkedListNode(val);
+
+      return;
+    }
+
+    const newNode = new LinkedListNode(val);
+
+    const temp = this.root;
+    this.root = newNode;
+    newNode.next = temp;
+    temp!.prev = newNode;
+  }
+
+  addAtTail(val: number): void {
+    if (!this.root) {
+      this.root = new LinkedListNode(val);
+
+      return;
+    }
+
+    const newNode = new LinkedListNode(val);
+
+    let node = this.root;
+    while (node.next) {
+      node = node.next;
+    }
+
+    node.next = newNode;
+    newNode.prev = node;
+  }
+
+  addAtIndex(index: number, val: number): void {
+    if (index === 0) {
+      this.addAtHead(val);
+
+      return;
+    }
+
+    const newNode = new LinkedListNode(val);
+
+    try {
+      const node = this.getNodeAt(index - 1);
+
+      const nextNode = node.next;
+      node.next = newNode;
+      newNode.prev = node;
+
+      if (nextNode) {
+        newNode.next = nextNode;
+        nextNode.prev = newNode;
+      }
+    } catch (e) {}
+  }
+
+  deleteAtIndex(index: number): void {
+    if (index === 0) {
+      this.root = this.root?.next ?? null;
+
+      return;
+    }
+
+    try {
+      const node = this.getNodeAt(index);
+
+      node.prev!.next = node.next;
+
+      if (node.next) {
+        node.next.prev = node.prev;
+      }
+    } catch (e) {}
+  }
+
+  private getNodeAt(index: number): LinkedListNode {
+    let node = this.root;
+
+    let i = 0;
+    while (i < index) {
+      node = node?.next ?? null;
+
+      i++;
+    }
+
+    if (node === null || i !== index) {
+      throw new Error(`You don't have value at index: ${index}`);
+    }
+
+    return node;
   }
 }
 
-class DoubleLinkedList {
-  head: ListNode | null;
+export class LinkedListNode {
+  value: number;
+  prev: LinkedListNode | null;
+  next: LinkedListNode | null;
 
-  constructor() {
-    this.head = null;
-  }
-
-  add(x: number): ListNode {
-    const newNode = new ListNode(x);
-
-    if (!this.head) {
-      this.head = newNode;
-
-      return newNode;
-    }
-
-    let n = this.head;
-    while (n.next) {
-      n = n.next;
-    }
-
-    n.next = newNode;
-    newNode.prev = n;
-
-    return newNode;
-  }
-
-  pop(): number {
-    let n = this.head;
-
-    if (!n.next) {
-      const popped = this.head.value;
-      this.head = null;
-
-      return popped;
-    }
-
-    while (n.next) {
-      n = n.next;
-    }
-
-    const popped = n.value;
-    n.prev.next = null;
-
-    return popped;
-  }
-
-  peek(): number {
-    let n = this.head;
-    if (!n.next) {
-      return n.value;
-    }
-
-    while (n.next) {
-      n = n.next;
-    }
-
-    return n.value;
-  }
-
-  remove(node: ListNode): number {
-    if (this.head === node) {
-      const value = this.head.value;
-
-      this.head = this.head.next;
-
-      return value;
-    }
-
-    let n = this.head;
-    while (n !== node && n.next) {
-      n = n.next;
-    }
-
-    if (n !== node) {
-      throw new Error("node must be found in this dll");
-    }
-
-    const value = n.value;
-
-    n.prev.next = n.next;
-    if (n.next) {
-      n.next.prev = n.prev;
-    }
-
-    return value;
+  constructor(value: number) {
+    this.value = value;
+    this.prev = null;
+    this.next = null;
   }
 }
